feb7289926bca95938797258f97474479ee51618
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2023-03-22 12:23:33 -0300

    aulas de json e os

--- aula_os/aula_os_shutil.py
+++ aula_os/aula_os_shutil.py
@@ -1,6 +1,10 @@
-# os + shutil - Copiando arquivos com Python
+# os + shutil - Apagando, copiando, movendo e renomeando pastas com Python
 # Vamos copiar arquivos de uma pasta para outra.
 # Copiar -> shutil.copy
+# Copiar Árvore recursivamente -> shutil.copytree
+# Apagar Árvore recursivamente -> shutil.rmtree
+# Apagar arquivos -> os.unlink
+# Renomear/Mover -> shutil.move ou os.rename
 import os
 import shutil
 
@@ -9,18 +13,23 @@ DESKTOP = os.path.join(HOME, 'Desktop')
 PASTA_ORIGINAL = os.path.join(DESKTOP, 'EXEMPLO')
 NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
 
-os.makedirs(NOVA_PASTA, exist_ok=True)
+shutil.rmtree(NOVA_PASTA, ignore_errors=True)
+shutil.copytree(PASTA_ORIGINAL, NOVA_PASTA)
+# shutil.move(NOVA_PASTA, NOVA_PASTA + '_EITA')
+shutil.rmtree(NOVA_PASTA, ignore_errors=True)
 
-for root, dirs, files in os.walk(PASTA_ORIGINAL):
-    for dir_ in dirs:
-        caminnho_novo_diretorio = os.path.join(
-            root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
-        )
-        os.makedirs(caminnho_novo_diretorio, exist_ok=True)
+# os.makedirs(NOVA_PASTA, exist_ok=True)
 
-    for file in files:
-        caminho_arquivo = os.path.join(root, file)
-        caminnho_novo_arquivo = os.path.join(
-            root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
-        )
-        shutil.copy(caminho_arquivo, caminnho_novo_arquivo)
+# for root, dirs, files in os.walk(PASTA_ORIGINAL):
+#     for dir_ in dirs:
+#         caminnho_novo_diretorio = os.path.join(
+#             root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
+#         )
+#         os.makedirs(caminnho_novo_diretorio, exist_ok=True)
+
+#     for file in files:
+#         caminho_arquivo = os.path.join(root, file)
+#         caminnho_novo_arquivo = os.path.join(
+#             root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
+#         )
+#         shutil.copy(caminho_arquivo, caminnho_novo_arquivo)
